14e3523740e2e51ae240828554776a3c32d2ccf2
kifixo <danielhuici@hotmail.com>
2023-02-20 20:09:05 +0100

    hidden videos detector for scrapper

--- scrapper.py
+++ scrapper.py
@@ -1,11 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from dbmanager import DBManager
+from gdrivemanager import GDriveManager
 import time
+import re
+
+
 
 print("HELLO WORLD")
 
 dbManager = DBManager()
+gdriveManager = GDriveManager()
 
 def get_actual_videoids(url, ocultos):
     browser = webdriver.Edge()
@@ -15,7 +20,9 @@ def get_actual_videoids(url, ocultos):
     browser.find_element(By.CSS_SELECTOR, "button[aria-label = 'Aceptar todo']").click()
     time.sleep(2)
     if ocultos:
-        browser.find_element(By.CSS_SELECTOR, "button[aria-label = 'Menú de acciones']").click()
+        element = browser.find_element(By.CSS_SELECTOR, "button[aria-label=\"Menú de acciones\"]")
+        browser.execute_script("arguments[0].click();", element)
+        print("ok")
         browser.find_element(By.LINK_TEXT, "Mostrar vídeos no disponibles").click()
 
     for _ in range (0, 10):
@@ -27,8 +34,9 @@ def get_actual_videoids(url, ocultos):
     links = [elem.get_attribute('href') for elem in youtube_elements]
     youtube_ids = []
     for link in links:
-        n_index = link.index("&")
-        youtube_ids.append(link[32:n_index])
+        if link != None:
+            n_index = link.index("&")
+            youtube_ids.append(link[32:n_index])
 
     return youtube_ids
 
@@ -40,16 +48,55 @@ def get_db_videoids():
 
     return ids
 
-all_yt_videoids = get_actual_videoids("https://www.youtube.com/playlist?list=PLaJq2Gw03Eigv6RQqXKmUP8lPO3IoXbEl", False)
+def get_files_videoids():
+    filetitles = gdriveManager.get_all_file_titles()
+    for index, value in enumerate(filetitles):
+        if value != "Lost and Found" and value != "DB Backups" and value != "27chart.db":
+            m = re.findall(r"\[([A-Za-z0-9_-]+)\]", value)
+            filetitles[index] = m[len(m) -1]  
+    return filetitles
 
-for id in all_yt_videoids:
-    if id == "h9wualcJuE4":
-        print("ESTÁ")
+def get_youtube_videoids():
+    playlists = dbManager.get_playlists()
+    all_yt_videoids = []
+    for playlist in playlists:
+        all_yt_videoids += get_actual_videoids("https://www.youtube.com/playlist?list=" + playlist.id, False)
+        print(f"Playlist: {playlist} scrapped")
 
-#print(all_yt_videoids)
-#print("-----")
-#result = set(all_yt_videoids).difference(get_db_videoids())
-#print(result)
+    return all_yt_videoids
 
+def get_hidden_playlist_videos(url):
+    list_without_hidden = get_actual_videoids(url, False)
+    list_with_hidden = get_actual_videoids(url, True)
+    return set(list_with_hidden).difference(list_without_hidden)
+
+
+print(get_hidden_playlist_videos("https://www.youtube.com/playlist?list=PLaJq2Gw03EijUkrVadSIA9Gxll7ewZ4oY"))
+"""    
+youtube_videoids = get_youtube_videoids()
+files_videoids = get_files_videoids()
+db_videoids = get_db_videoids()
+
+
+result = set(youtube_videoids).difference(files_videoids)
+result2 = set(youtube_videoids).difference(db_videoids)
+result3 = set(db_videoids).difference(youtube_videoids)
+result4 = set(db_videoids).difference(files_videoids)
+result5 = set(files_videoids).difference(db_videoids)
+result6 = set(files_videoids).difference(youtube_videoids)
+print(result)
+print("---")
+print(result2)
+print("---")
+print(result3)
+print("---")
+print(result4)
+print("---")
+print(result5)
+print("---")
+print(result6)
+print("---")
+
+"""
 
     
